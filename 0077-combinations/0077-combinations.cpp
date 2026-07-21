262374d9f7e76562dde8f8d1d2de1bbfed37a754
Aditya Kumar <kumar17aaditya@gmail.com>
2026-07-21 23:53:00 +0530

    Time: 68 ms (54.61%), Space: 111.5 MB (18.97%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void combinations(vector<int>& temp, vector<vector<int>>& res, int sin,
+                      int ein, int k) {
+        if (sin > ein) {
+            if (k == 0) {
+                res.push_back(temp);
+                return;
+            }
+            return;
+        }
+        temp.push_back(sin);
+        sin +=1;
+        combinations(temp, res, sin, ein, k - 1);
+        temp.pop_back();
+        combinations(temp, res, sin, ein, k);
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        int sin = 1;
+        combinations(temp, res, sin, n, k);
+        return res;
+    }
+};
